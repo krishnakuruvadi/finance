9261a6521eead9bc7955fe2f0494ee51ba68e447
krishnakuruvadi <krishnakuruvadi@gmail.com>
2020-04-17 01:37:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 if [ ! -d "venv" ]; then
   python3 -m venv ./venv
   . ./venv/bin/activate
-  pip3 install quandl PyPDF2
+  pip3 install quandl PyPDF2 xlrd autopep8 requests
 else
   . ./venv/bin/activate
 fi
